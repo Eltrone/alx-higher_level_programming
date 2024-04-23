9c2f66379d27da8bb3de341b819bc5bc03c9528b
Eltrone <amineelorche@gmail.com>
2024-04-23 06:15:58 -0700

    file added

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,33 @@
 #!/usr/bin/node
+// Script to count the number of films featuring the character Wedge Antilles (character ID 18).
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = '18';
+if (process.argv.length !== 3) {
+  console.error('Usage: ./4-starwars_count.js <API URL>');
+  process.exit(1);
+}
 
-request(apiUrl, (error, response, body) => {
+const apiUrl = process.argv[2];
+
+request(apiUrl, { json: true }, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    console.log(count);
+    return console.log(error);
   }
+
+  if (response.statusCode !== 200) {
+    return console.log('Failed to retrieve data from API');
+  }
+
+  let count = 0;
+  const films = body.results;
+
+  films.forEach(film => {
+    const characters = film.characters;
+    if (characters.includes(`https://swapi-api.alx-tools.com/api/people/18/`)) {
+      count++;
+    }
+  });
+
+  console.log(count);
 });
