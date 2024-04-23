e0dadd87285322160b2fbe6d4eefa4a82f607fc9
Eltrone <amineelorche@gmail.com>
2024-04-23 06:13:01 -0700

    file added

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -12,15 +12,23 @@ request(apiUrl, (error, response, body) => {
     const movie = JSON.parse(body);
     const charactersUrls = movie.characters;
 
-    charactersUrls.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
+    // Fonction récursive pour afficher les personnages dans le bon ordre
+    const printCharacters = (index) => {
+      if (index >= charactersUrls.length) {
+        return;
+      }
+      request(charactersUrls[index], (error, response, body) => {
         if (error) {
           console.error(error);
         } else {
           const character = JSON.parse(body);
           console.log(character.name);
+          printCharacters(index + 1); // Appel récursif pour passer au personnage suivant
         }
       });
-    });
+    };
+
+    // Commencer par le premier personnage
+    printCharacters(0);
   }
 });
